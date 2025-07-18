da561494ecd3cb8f3c723b02428348451c0cedc7
EJ Faye Dulay <efadulay@usm.edu.ph>
2025-07-18 17:07:32 +0800

    put dashboard

--- layouts/home.vue
+++ layouts/home.vue
@@ -1,5 +1,4 @@
 <script setup>
-// filepath: c:\Users\ejfay\FreshBytes\freshbytes-admin\layouts\home.vue
 import { onMounted } from 'vue'
 import { useFlowbite } from '~/composables/useFlowbite';
 
@@ -51,10 +50,10 @@ onMounted(() => {
                             id="dropdown-user">
                             <div class="px-4 py-3" role="none">
                                 <p class="text-sm text-gray-900 dark:text-white" role="none">
-                                    Neil Sims
+                                    Admin
                                 </p>
                                 <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                                    neil.sims@flowbite.com
+                                    admin@gmail.com
                                 </p>
                             </div>
                             <ul class="py-1" role="none">
@@ -91,6 +90,22 @@ onMounted(() => {
         aria-label="Sidebar">
         <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
             <ul class="space-y-2 font-medium">
+                <!-- Dashboard -->
+                <li>
+                    <NuxtLink to="/home"
+                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                            viewBox="0 0 22 21">
+                            <path
+                                d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                            <path
+                                d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                        </svg>
+                        <span class="ms-3">Dashboard</span>
+                    </NuxtLink>
+                </li>
+
                 <!-- Users Dropdown -->
                 <li>
                     <button type="button" 
